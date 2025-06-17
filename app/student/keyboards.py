c64396b34c0f5fbcfc276fcf1358f89b9102782e
Begzod10 <rimefara22@gmail.com>
2025-06-17 22:35:38 +0500

    tasks celery

--- app/student/keyboards.py
+++ app/student/keyboards.py
@@ -69,7 +69,7 @@ def create_years_reply_keyboard(data):
             row = []
     if row:  # Add any remaining year (odd number of years)
         keyboard.append(row)
-    row.append(KeyboardButton(text="⬅️ Ortga qaytish"))
+    keyboard.append(KeyboardButton(text="⬅️ Ortga qaytish"))
     return ReplyKeyboardMarkup(
         keyboard=keyboard,
         resize_keyboard=True,
