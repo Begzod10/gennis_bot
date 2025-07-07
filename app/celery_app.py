40fe1a0c2b3f51195dee9312e778d31b5ddf20a4
Begzod10 <rimefara22@gmail.com>
2025-07-07 11:53:27 +0500

    checking celery task

--- app/celery_app.py
+++ app/celery_app.py
@@ -13,19 +13,19 @@ celery = Celery(
     backend=os.getenv('CELERY_RESULT_BACKEND')
 )
 
-# celery.conf.beat_schedule = {
-#     'send-balance-every-12-hours': {
-#         'task': 'app.tasks.send_balance_to_users',
-#         'schedule': crontab(minute=0, hour='0,12'),
-#     },
-# }
-
 celery.conf.beat_schedule = {
-    'send-balance-every-minute': {
+    'send-balance-every-12-hours': {
         'task': 'app.tasks.send_balance_to_users',
-        'schedule': crontab(),  # This runs the task every minute
+        'schedule': crontab(minute=0, hour='0,12'),
     },
 }
+
+# celery.conf.beat_schedule = {
+#     'send-balance-every-minute': {
+#         'task': 'app.tasks.send_balance_to_users',
+#         'schedule': crontab(),  # This runs the task every minute
+#     },
+# }
 
 celery.conf.timezone = 'Asia/Tashkent'  # Optional but recommended
 from app import tasks
