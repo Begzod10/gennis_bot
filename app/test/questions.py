c1d0609e73c8fd297c1b5753bd03f3c516055f47
shohjahon690 <shoxjahonsobirjonov777@gmail.com>
2025-11-12 18:26:16 +0500

    Yangi o'zgarishlar

--- app/test/questions.py
+++ app/test/questions.py
@@ -1,9 +1,9 @@
-GENNIS_TOKEN = os.getenv(
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc2Mjg1NjY4NCwianRpIjoiYmFhZTczZTAtNmU4Yy00NDU1LWEwYTktNGU4NjYzMzhiNjY5IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjhkMDI3NzJjNTE5MzExZjBhNTQ4MzFkODQyNzBhMjIwIiwibmJmIjoxNzYyODU2Njg0LCJjc3JmIjoiZmY2OGFkZDktNGEyOS00OGZiLWFmMzEtMDFjZDliNmE2ZGVhIiwiZXhwIjoxNzYyOTQzMDg0fQ.kyqMRFUS8AcPDqMJ8yLD0duqw0Q0wwiGgGgAkcfdKlc")
-TEST_LIST_URL = "https://classroom.gennis.uz/api/pisa/test/crud/34"
-active_questions = {}
-HEADERS = lambda token: {
-    "Authorization": f"Bearer {token}",
-    "Accept": "application/json",
-    "User-Agent": "GennisBot/1.0"
-}
+# GENNIS_TOKEN = os.getenv(
+#     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTc2Mjg1NjY4NCwianRpIjoiYmFhZTczZTAtNmU4Yy00NDU1LWEwYTktNGU4NjYzMzhiNjY5IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IjhkMDI3NzJjNTE5MzExZjBhNTQ4MzFkODQyNzBhMjIwIiwibmJmIjoxNzYyODU2Njg0LCJjc3JmIjoiZmY2OGFkZDktNGEyOS00OGZiLWFmMzEtMDFjZDliNmE2ZGVhIiwiZXhwIjoxNzYyOTQzMDg0fQ.kyqMRFUS8AcPDqMJ8yLD0duqw0Q0wwiGgGgAkcfdKlc")
+# TEST_LIST_URL = "https://classroom.gennis.uz/api/pisa/test/crud/34"
+# active_questions = {}
+# HEADERS = lambda token: {
+#     "Authorization": f"Bearer {token}",
+#     "Accept": "application/json",
+#     "User-Agent": "GennisBot/1.0"
+# }
