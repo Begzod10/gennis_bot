31ef431a5f010c0d493733c0a1d77da4d02bf425
Begzod10 <rimefara22@gmail.com>
2026-03-10 13:21:12 +0500

    commit

--- app/student/handlers.py
+++ app/student/handlers.py
@@ -35,7 +35,7 @@ QUESTION_TIME = 15
 
 
 
-@student_router.message(F.text.startswith("💳 To’lovlar ro’yhati"))
+@student_router.message(StateFilter("*"), F.text.startswith("💳 To’lovlar ro’yhati"))
 async def get_payments_list(message: Message, state: FSMContext):
     api = os.getenv('API')
     telegram_user = message.from_user
